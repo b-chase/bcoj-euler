2e10f143810c691e210689871b4812ac1a6f677a
Brian <46642841+b-chase@users.noreply.github.com>
2023-07-04 08:28:17 -0400

    solved p1

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from solutions import problem1
+from time import time_ns
 
+start_ns = time_ns()
 
 problem1.solve()
 
+elapsed = (time_ns() - start_ns)/1e3
+print(f'\nSolved in {elapsed:.5f} us')
--- solutions/problem1.py
+++ solutions/problem1.py
@@ -4,4 +4,24 @@
 """
 
 def solve():
-    print('solved!')
+    N = 1000
+
+    # fast
+    n_3 = (N-1)//3
+    n_5 = (N-1)//5
+    n_15 = (N-1)//15
+
+    tot_3 = 3*(n_3 * (n_3+1))//2
+    tot_5 = 5*(n_5 * (n_5+1))//2
+    tot_15 = 15*(n_15 * (n_15+1))//2
+    print(f"{tot_3} + {tot_5} - {tot_15} = {tot_3+tot_5-tot_15}")
+
+    # slow
+    # total = 0
+    # for i in range(1, N):
+    #     if i % 3 == 0:
+    #         total += i
+    #     elif i % 5 == 0:
+    #         total += i
+        
+    # print(total)
