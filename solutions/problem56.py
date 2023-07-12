bf8583a059b29d314bb6a66c3c9b6e7e190f8ab4
b-chase <bcrawfc@gmail.com>
2023-07-11 23:21:31 -0400

    Create problem56.py

---
+++ solutions/problem56.py
@@ -0,0 +1,12 @@
+"""<p>A googol ($10^{100}$) is a massive number: one followed by one-hundred zeros; $100^{100}$ is almost unimaginably large: one followed by two-hundred zeros. Despite their size, the sum of the digits in each number is only $1$.</p>
+<p>Considering natural numbers of the form, $a^b$, where $a, b \lt 100$, what is the maximum digital sum?</p>
+
+"""
+
+import euler_math as em
+
+def solve(debug=False):
+    
+    res=None
+    
+    print(f"*** Answer: {res} ***")
