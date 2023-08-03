25e45ec66776792c686f5d15a0556a2d61c6a82c
b-chase <bcrawfc@gmail.com>
2023-08-03 13:23:05 -0400

    solved 81 and modified loading script

--- meta/next.py
+++ meta/next.py
@@ -2,6 +2,7 @@ import pandas as pd
 from argparse import ArgumentParser
 import requests
 import os
+import re
 
 
 ap = ArgumentParser()
@@ -13,16 +14,37 @@ cli_pno = args['problem_number']
 
 def load_problem(pnum:int):
     problem_url = f"https://projecteuler.net/minimal={pnum}"
-    problem_html = requests.get(url=problem_url).content
+    problem_html = requests.get(url=problem_url).content.decode()
     try:
+        prob_text_file = re.search(r'href\=\"(.+?txt)', problem_html).groups()[0]
+        if prob_text_file:
+            file_link = f"https://projecteuler.net/{prob_text_file}"
+            print(file_link)
+            file_content = requests.get(file_link).content.decode()
+            with open(f"solutions/problem{pnum}.txt", "x") as t:
+                t.write(file_content)
+        # raise Exception("Check for text file!")
         with open(f'solutions/problem{pnum}.py', 'x') as f:
             try:
                 f.write(f"# https://projecteuler.net/problem={pnum}\n")
-                f.write(f'''"""{problem_html.decode()}"""\n\n''')
+                f.write(f'''"""{problem_html}"""\n\n''')
+                
             except Exception as e:
                 f.write(f"# There was a problem loading the HTML for this problem\n\n")
                 print(e)
-            f.write('''import euler_math as em\n\ndef solve(debug=False):\n    \n    res=None\n    \n    print(f"*** Answer: {res} ***")''')
+            indent = "    "
+            starting_code = [
+                "import euler_math as em",
+                "",
+                "def solve(debug=False):",
+                indent,
+                indent+"res=None",
+                indent,
+                indent+"print(f'*** Answer: {res} ***')"
+            ]
+            if prob_text_file:
+                starting_code.insert(4, indent+f"with open('solutions/problem{pnum}.txt') as f:\n"+2*indent+"pass\n"+indent)
+            f.write('\n'.join(starting_code))
     except FileExistsError:
         print("The specified problem file already exists!")
     os.system(f"code solutions/problem{pnum}.py -r")
